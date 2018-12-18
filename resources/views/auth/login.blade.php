489bed02ad9f4105f639abf8cb8edcdee01de197
Arigato7 <plstsale@gmail.com>
2018-12-18 16:58:36 +0400

    Изменил страницы входа и регистрации

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,9 +4,8 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+            <h1 class="h1 mb-4">Вход</h1>
             <div class="card">
-                <div class="card-header">Вход</div>
-
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
