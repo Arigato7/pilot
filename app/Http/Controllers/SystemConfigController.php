0b9118f72b31cd9fa499d28a23651c2997b1f99e
Arigato7 <plstsale@gmail.com>
2019-01-31 12:40:26 +0400

    Добавил файлы для настроек системы

---
+++ app/Http/Controllers/SystemConfigController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Pilot\Http\Controllers;
+
+use Pilot\SystemConfig;
+use Illuminate\Http\Request;
+
+class SystemConfigController extends Controller
+{
+    
+    public function index() {
+        
+    }
+}
